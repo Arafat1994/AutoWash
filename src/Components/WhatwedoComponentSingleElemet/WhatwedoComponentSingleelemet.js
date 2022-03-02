ca59bafa420618ef52f6a4fab71efd0ae9da443f
arafat <arafat1994@outlook.sa>
2022-03-02 14:25:19 +0300

    edit redux Links

--- src/Components/WhatwedoComponentSingleElemet/WhatwedoComponentSingleelemet.js
+++ src/Components/WhatwedoComponentSingleElemet/WhatwedoComponentSingleelemet.js
@@ -6,7 +6,7 @@ const WhatwedoComponentSingleelemet = (props) => {
     const { id, imgsrc, header, somedetail } = props;
 
     return (
-        <Link to={`/Service/id=${id}&:service=${header}`} className="whatwedoelement"  key={id} >
+        <Link to={`/Service/id=${id}&service=${header}`} className="whatwedoelement"  key={id} >
             <div className="Whatwedoeledivimg">  <img src={require('./../../Assets/images' + imgsrc).default} alt="carwash" /> </div>
             <div className="whatwedoelementtext">
                 <p> {header} </p>
